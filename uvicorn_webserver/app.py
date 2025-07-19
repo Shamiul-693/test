723350a8f4e5098a60fd582c58a705bc41c9f83b
Shamiul-693 <shamiulislam693@gmail.com>
2025-07-19 12:04:02 +0600

    uvicorn werserver

---
+++ uvicorn_webserver/app.py
@@ -0,0 +1,17 @@
+async def app(scope, receive, send):
+    if scope['type'] == 'http':
+        response_body = b"this is mr samiul islam"
+        headers = [(b'content-type', b'text/plain')]
+        
+        await send({
+            'type': 'http.response.start',
+            'status': 200,
+            'headers': headers,
+        })
+        
+        await send({
+            'type': 'http.response.body',
+            'body': response_body,
+        })
+    else:
+        raise NotImplementedError("Only HTTP scope is supported")
